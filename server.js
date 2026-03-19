f0bfbf7522fb20883971335c5378f97103c0eb64
PRADHUMAN SINGH <115773754+PRADHUMAN-SINGH-1@users.noreply.github.com>
2026-03-19 16:18:05 +0530

    Update server.js chnged port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 // helper
 const safeRequest = async (url, options = {}) => {
